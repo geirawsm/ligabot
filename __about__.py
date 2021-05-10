644ebd69f712fd4cac7fa34f977f55866920a924
geirawsm <geira@pm.me>
2021-05-10 10:36:53 +0200

    Test-commit from VS Code

---
+++ __about__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+about = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__"
+]
+
+__title__ = "ligabot"
+__summary__ = "reddit bot for /r/NorskFotball"
+__uri__ = "reddit.com/r/norskfotball"
+
+__version__ = "0.0.1"
+
+__author__ = "armandg"
+__email__ = "armandg+ligabot@gmail.com"
+
+__license__ = "GPL-v3.0"
